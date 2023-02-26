2d145c267aafc030696927c5ebdb62a7be206c7a
DinarG91 <low-d-low@yandex.ru>
2023-02-26 17:46:31 +0700

    Исправил название инпут

--- script/cats.js
+++ script/cats.js
@@ -5,7 +5,7 @@ const cats = [
         "img_link": "https://img.freepik.com/free-photo/gray-kitty-with-monochrome-wall-behind-her_23-2148955126.jpg",
         "age": 8,
         "rate": 3,
-        "favourite": true,
+        "favorite": true,
         "description": "Марсик – британцы джентльмены, сиамы своенравны, а экзоты просто душки"
     },
     {
@@ -14,7 +14,7 @@ const cats = [
         "img_link": "https://img.freepik.com/free-photo/adorable-kitty-with-monochrome-wall-behind-her_23-2148955140.jpg",
         "age": 2,
         "rate": 4,
-        "favourite": false,
+        "favorite": false,
         "description": "Лео - сиамский кот, характерной чертой этих домашних любимцев являются громкий голос с необычными интонациями и тяга к «вербальному» общению с людьми. Легко узнаваемы благодаря контрастному окрасу и выразительным бирюзовым глазам. Косоглазие и заломы на хвосте, ранее не считавшиеся недостатками."
     },
     {
@@ -23,7 +23,7 @@ const cats = [
         "img_link": "https://img.freepik.com/free-photo/cute-scottish-straight-gray-cat-hunting-and-playing_574295-452.jpg",
         "age": 1,
         "rate": 5,
-        "favourite": true,
+        "favorite": true,
         "description": "Беко - порода Египетская мау, кот с нежностью и любовью ко всем членам семьи, а особенно – к человеку, которого считают хозяином."
     },
     {
@@ -32,7 +32,7 @@ const cats = [
         "img_link": "https://img.freepik.com/free-photo/white-cat-lies-on-woman-s-knees_8353-539.jpg",
         "age": 4,
         "rate": 7,
-        "favourite": false,
+        "favorite": false,
         "description": "Илла - порода японский бобтейл, аборигенная кошка из Страны восходящего солнца с коротким помпоновидным хвостом. Короткие пушистые хвостики японских бобтейлов очень чувствительны, поэтому любое неосторожное прикосновение к этой части тела причиняет животному боль и дискомфорт. Еще сильны охотничьи инстинкты, поэтому они любят ловить насекомых и мелких зверьков. "
     },
     {
@@ -41,7 +41,7 @@ const cats = [
         "img_link": "https://img.freepik.com/premium-photo/anthropomorphic-black-cat-with-elegant-suit-portrait-3d-rendering_691560-3314.jpg",
         "age": 1,
         "rate": 8,
-        "favourite": false,
+        "favorite": false,
         "description": "Ями - черный кот, всегда считалась мистическим животным, загадочным, поэтому и кот отводится роль некого проводника между мирами. Домашный любимец сразу понимает, что люди приносят в дом еду, кормят, убирают, играют. Она с удовольствием крутится возле людей, и, вообще довольно общительна." 
     },
     {
@@ -50,7 +50,8 @@ const cats = [
         "img_link": "https://img.freepik.com/free-photo/copy-space-cute-cat-wearinf-fur-cap-in-winter_23-2148348037.jpg",
         "age": 6,
         "rate": 10,
-        "favourite": false,
+        "favorite": false,
         "description": "Муррфик - флегматик! Кот спокойный и медлительный, низкая активность играет с ним злую шутку: кот-тихоня склонен к полноте"
     }
 ]
+
